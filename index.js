2d2242271a62e49076c713c167e031a7f45f9185
niclbody13 <91341366+niclbody13@users.noreply.github.com>
2024-07-30 23:09:57 -0700

    update path

--- index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.body.style.overflow = 'hidden';
-    var content = document.querySelector(".content");
-    content.classList.add("animate");
-    content.addEventListener('animationend', function() {
-        document.body.style.overflow = '';
-    });
-});
